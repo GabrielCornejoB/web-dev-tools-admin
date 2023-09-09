060ff638f050db8a62aad9f888d40746085c6cad
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-09-09 14:50:47 -0500

    After 3 hours of suffering i found compatible versions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'web-dev-tools-admin';
+
+  constructor(private firestore: Firestore) {
+    this.getData();
+  }
+
+  getData() {
+    const collectionInstance = collection(this.firestore, 'tools');
+    collectionData(collectionInstance).subscribe((data) => console.log(data));
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'web-dev-tools-47601',
+    appId: '1:844949998980:web:d57d51cf1bbf80252521d1',
+    storageBucket: 'web-dev-tools-47601.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyD5pIM1TmPtHuSZupMRvfRhlgHz5Tt1rlM',
+    authDomain: 'web-dev-tools-47601.firebaseapp.com',
+    messagingSenderId: '844949998980',
+  },
+};
