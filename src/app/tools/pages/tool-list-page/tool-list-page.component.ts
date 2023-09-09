0398c29859f01bc7ad14dbc9c4c31c90485f90da
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-09-09 12:08:44 -0500

    Table base design

--- src/app/tools/pages/tool-list-page/tool-list-page.component.ts
+++ src/app/tools/pages/tool-list-page/tool-list-page.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { Tool } from '../../models/tool.model';
 
 @Component({
   selector: 'wdt-list-page',
   templateUrl: './tool-list-page.component.html',
 })
-export class ToolListPageComponent {}
+export class ToolListPageComponent {
+  public tools: Tool[] = [];
+}
