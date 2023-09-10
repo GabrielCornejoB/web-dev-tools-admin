fa798b77bacd77168ea0bd7005cda8ed55f0bd26
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-09-09 22:09:00 -0500

    Unsubscribe events

--- src/app/tools/components/preview-card/preview-card.component.ts
+++ src/app/tools/components/preview-card/preview-card.component.ts
@@ -1,19 +1,24 @@
-import { Component, Input, OnInit, inject } from '@angular/core';
+import { Component, Input, OnInit, inject, OnDestroy } from '@angular/core';
 import { ToolDTO } from '../../models/tool.model';
 import { ToolsService } from '../../services/tools.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'wdt-preview-card',
   templateUrl: './preview-card.component.html',
 })
-export class PreviewCardComponent implements OnInit {
+export class PreviewCardComponent implements OnInit, OnDestroy {
   private toolsService = inject(ToolsService);
 
-  tool: ToolDTO | undefined;
+  public tool: ToolDTO | undefined;
+  private subscription: Subscription = new Subscription();
 
   ngOnInit(): void {
-    this.toolsService.tool$.subscribe((data) => {
+    this.subscription = this.toolsService.tool$.subscribe((data) => {
       this.tool = data;
     });
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/tools/pages/tool-list-page/tool-list-page.component.ts
+++ src/app/tools/pages/tool-list-page/tool-list-page.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { Tool } from '../../models/tool.model';
 import { ToolsService } from '../../services/tools.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'wdt-list-page',
   templateUrl: './tool-list-page.component.html',
 })
-export class ToolListPageComponent implements OnInit {
+export class ToolListPageComponent implements OnInit, OnDestroy {
   private toolsService = inject(ToolsService);
   public tools: Tool[] = [];
+
+  private toolsSubscription: Subscription = new Subscription();
   public isLoading: boolean = false;
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.toolsService.getAll().subscribe((data) => {
+    this.toolsSubscription = this.toolsService.getAll().subscribe((data) => {
       this.tools = data;
       this.isLoading = false;
     });
+  }
+  ngOnDestroy(): void {
+    this.toolsSubscription.unsubscribe();
   }
 }
