47da99e096a623fccbf064096e13984194ab3206
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-10-15 13:51:49 -0500

    Angular Material setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './auth/auth-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    loadChildren: () => import('./auth/auth.routes').then((r) => r.routes),
+  },
+];
