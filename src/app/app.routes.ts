1669abcfc5feba01362591c69b3d579ec0a1970b
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-10-15 15:48:53 -0500

    Path shortcuts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { AuthLayoutComponent } from './auth/auth-layout.component';
+import { AuthLayoutComponent } from '@auth/auth-layout.component';
 
 export const routes: Routes = [
   {
     path: 'auth',
     component: AuthLayoutComponent,
-    loadChildren: () => import('./auth/auth.routes').then((r) => r.routes),
+    loadChildren: () => import('@auth/auth.routes').then((r) => r.routes),
   },
 ];
