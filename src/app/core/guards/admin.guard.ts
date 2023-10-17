3e89f0aa86edaa401f99be271dc2f892edb12b6c
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-10-16 19:06:28 -0500

    Added comments & specified return values

--- src/app/core/guards/admin.guard.ts
+++ src/app/core/guards/admin.guard.ts
@@ -2,7 +2,10 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '@core/services';
 
-export const adminGuard: CanActivateFn = (route, state) => {
+/**
+ * Guard that allows entering the Admin feature module
+ */
+export const adminGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -14,14 +14,24 @@ import { User } from '@core/models';
 })
 export class AuthService {
   private user: WritableSignal<User | null> = signal(null);
+  /** Computed signal that returns the current login status of the user */
   public isLoggedIn: Signal<boolean> = computed(() => !!this.user());
 
   constructor() {}
 
-  public setUser(user: User) {
+  /**
+   * Function that sets the logged User into a global signal that then
+   * has a computed signal that globally shares the log in status
+   * @param user User object to login
+   */
+  public setUser(user: User): void {
     this.user.set(user);
   }
-  public removeUser() {
+  /**
+   * Function that sets the logged user to null, so the public computed
+   * signal ('isLoggedIn') will be false
+   */
+  public removeUser(): void {
     this.user.set(null);
   }
 }
