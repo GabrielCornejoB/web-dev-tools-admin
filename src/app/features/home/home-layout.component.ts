b08b657bd853647d93ebbea98a8c2267306e5204
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-11-21 21:39:25 -0500

    Toolbar

--- src/app/features/home/home-layout.component.ts
+++ src/app/features/home/home-layout.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'wdt-home-layout',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule,
+  ],
   templateUrl: './home-layout.component.html',
-  styleUrls: ['./home-layout.component.scss']
+  styleUrls: ['./home-layout.component.scss'],
 })
-export class HomeLayoutComponent {
-
-}
+export class HomeLayoutComponent {}
